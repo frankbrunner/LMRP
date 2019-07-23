4dbd79790e07685e51601035dcfe70997f24a758
Douy <frank.brunner@lancos.ch>
2019-07-23 22:02:07 +0000

    next commit

--- MCP.py
+++ MCP.py
@@ -3,8 +3,10 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 from Ultrasonic import Ultrasonic
+from movingCar import movingCar	
 
 ultrasonic = Ultrasonic(12,24,1)
-ultrasonic.startMeasure()
-ultrasonic.stopMeasure()
+#ultrasonic.startMeasure()
 
+move = movingCar(17,27,23,24)
+move.forward()
