06994e4c82d24534ef863aa4c4c388eb10678a52
Frank <frank.brunner@lancos.ch>
2019-08-02 23:53:05 +0200

    first commit

--- MCP.py
+++ MCP.py
@@ -4,9 +4,22 @@ import time
 GPIO.setmode(GPIO.BCM)
 from Ultrasonic import Ultrasonic
 from movingCar import movingCar	
+GPIO.setwarnings(False)
 
 #ultrasonic = Ultrasonic(12,24,1)
 #ultrasonic.startMeasure()
 
 move = movingCar(17,27,23,24)
-move.forwardDistance(3500.0)
+move.forwardDistance(350.0)
+
+delay = 5
+timer = 0
+
+while timer < delay:
+	time.sleep(1)
+	timer = timer + 1
+	print("delay" + str( timer))
+	
+
+if timer >= delay:
+	move.stop()
--- movingCar.py
+++ movingCar.py
@@ -8,7 +8,7 @@ class movingCar():
 	DIST_PER_SEC = 35.0
 	
 	def __init__(self, left_pin1, left_pin2, right_pin1, right_pin2):
-		
+		#17,27,23,24
 		self.MOTOR_LEFT_PIN1 = left_pin1
 		self.MOTOR_LEFT_PIN2 = left_pin2
 		self.MOTOR_RIGHT_PIN1 = right_pin1
@@ -18,13 +18,12 @@ class movingCar():
 		GPIO.setup(self.MOTOR_LEFT_PIN2, GPIO.OUT)
 		GPIO.setup(self.MOTOR_RIGHT_PIN1, GPIO.OUT)
 		GPIO.setup(self.MOTOR_RIGHT_PIN2, GPIO.OUT)
-		
-	    
-		
+			
 	def forward(self):
-		GPIO.output(self.MOTOR_RIGHT_PIN1, GPIO.HIGH)
+		print ("get forward")
+		GPIO.output(self.MOTOR_RIGHT_PIN1, GPIO.LOW)
 		GPIO.output(self.MOTOR_RIGHT_PIN2, GPIO.HIGH)
-		GPIO.output(self.MOTOR_LEFT_PIN1, GPIO.HIGH)
+		GPIO.output(self.MOTOR_LEFT_PIN1, GPIO.LOW)
 		GPIO.output(self.MOTOR_LEFT_PIN2, GPIO.HIGH)
 		
 	def stop(self):
