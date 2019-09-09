7575a8a16ad2253b5a250d8f790b8f9c90ad7f59
Frank <frank.brunner@lancos.ch>
2019-09-09 23:27:28 +0200

    commit

--- MCP.py
+++ MCP.py
@@ -2,6 +2,8 @@
 import RPi.GPIO as GPIO
 import time
 import threading
+import csv
+import os
 GPIO.setmode(GPIO.BCM)
 from Ultrasonic import ultrasonic
 from movingCar import movingCar	
@@ -35,8 +37,7 @@ longitude = 0.0
 latitude = 0.0
 position = {}
 gpsQuality = 0.0 
-triggerSaveWaypoints = 0
-initialPositionSet = False
+menue = "main"
 
 
 def scanFrontforBounderys(currentPosition):
@@ -49,45 +50,83 @@ def loadGpsData():
      longitude = gpsData.getLongitude()
      latitude = gpsData.getLatitude()
      gpsQuality = gpsData.getGPSQual()
-     
-def defineWaypoints():
-     global triggerSaveWaypoints
-     if gpsData.qual >= 1:
-          print("Do you want to start with define Waypoints yes/no:")
-          inputValue = input()
-          if inputValue == "yes":
-               triggerSaveWaypoints = 1
-     else:
-          print("low GPS Quality:" + str(gpsQuality))
           
-def saveWaypoints():
-     file = open(waypoint.csv, w)
-     global initialPositionSet
-     if initialPositionSet == False:
+def gpsInitalizing():
+     clearConsole()
+     print("Set robot to first waypoint")
+     action = input()
+     if action != None:
           #set inital position
           i = 0
-          position[i] = [float(gpsData.lng),float(gpsData.lat)]
-          initialPositionSet = True
-     if initialPositionSet == True:
-          print("Set next gps point type yes:")
-          inputValue = input()
-          x = len(position)
-          position[x] = [gpsData.lng,gpsData.lat]
+          position[i] = [gpsData.lng,gpsData.lat]
+          while True:
+               clearConsole()
+               i = len(position)
+               print (position)
+               print("1. Set next Waypoint" + "\n" 
+                     "2. if completd")
+               action = int(input())
+               if action == 1:
+                    position[i] = [gpsData.lng,gpsData.lat]
+                    
+               if action == 2:
+                    saveToCsv(position)
+                    break
+          getMainMenue()
           
-          print (position)
+def saveToCsv(data):
+     with open('waypoints.csv', 'w', ) as csvFile:
+          writer = csv.writer(csvFile)
+          writer.writerow(data)
+     csvFile.close()
+          
+def clearConsole():
+     clear = lambda: os.system("clear")
+     clear()
+          
+def getMainMenue():
+     global menue 
+     clearConsole()
+     print("1. Start cuttin Gras" +"\n" 
+           "2. Initial field" + "\n" 
+           "3. Exit")
+     action = int(input())
+     #auswählen der Hautfunktion
+     if action == 1:
+          menue = "cuttin"
+     if action == 2:
+          menue = "initial"
+     if action == 3:
+          exit()
 
+def getCuttingMenue():
+     clearConsole()
+     print(1)
+     
+def getInitialMenue():
+     clearConsole()
+     print("1. Start initalizing" +"\n" 
+           "2. End initializing")
+     action = int(input())
+     if action == 1:
+          gpsInitalizing()
+     if action == 2:
+          getMainMenue()    
+          
+def GPSMainLoop():
+     while True:
+          print(menue)
+          if menue == "main":
+               getMainMenue()
+          if menue == "initial":
+               getInitialMenue()
+          
+          time.sleep(0.1)  
+          
 def GPSdataLoop():
      while True:
           gpsData.getGPSData()
           time.sleep(0.5) 
-          
-def GPSMainLoop():
-     while True:
-          if triggerSaveWaypoints == 1:
-               saveWaypoints()
-          else:
-               defineWaypoints()
-          time.sleep(0.1)           
           
     
 process01 = threading.Thread(target=GPSdataLoop)
